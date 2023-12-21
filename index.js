ebfb0df0b2af0731133939919f4d3cb810c9ae28
gurunganita <gurunganita982@gmail.com>
2023-12-21 12:08:26 +0300

    variable-interactions-work

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 console.log("Hello World!");
+
+// Es5
+// global variables
+var personn = "Meerub";
+person = "Yumna";
+
+//  Es6
+// Let allows you to reassign the variable
+let person1 = "Ram";
+person1 = "Hari";
+
+//  const refers to constant so it does not allow u
+// us to change the variable
+
+const person2 = "sia";
+//person2 = "sita";
+// ctrl + / (comment)
+
+const pi = 3.1475;
+
+console.log(person2);
+
+// Interactions
+alert("hi!");
+const form1 = prompt("what is your name");
+const form = confirm("are you veg or non-veg");
+console.log(form1, form);
