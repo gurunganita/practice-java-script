6aff0fa976387ccfff8d108d347bd560ee07be31
gurunganita <gurunganita982@gmail.com>
2023-12-21 12:22:57 +0300

    operations-practice

---
+++ operations-statements/operations.js
@@ -0,0 +1,16 @@
+/*
+Logical Operations
+
+-AND (&&)
+-OR (||)
+-NOT (!)
+
+*/
+
+const num1 = 8;
+const num2 = 10;
+
+if (num1 < 5 || num2 > 1) {
+  // alert("hello");
+  alert(num1 + "is greater than " + num2);
+}
